28dda5ae3e83d028b5f1b73eb55ef97d24945c6b
arnav <arnav4c@gmail.com>
2023-09-15 15:56:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const linksPanel = document.querySelector('.links-panel')
+
+const links = [
+    {image: 'assests/icons/instagram.png', title: 'Instagram', href: "https://www.instagram.com/arnavbuilds/"},
+    {image: 'assests/icons/discord.png', title: 'Discord', href: 'https://discord.gg/BGgkrfkh'},
+    {image: 'assests/icons/reddit.png', title: 'Reddit', href: 'https://www.reddit.com/user/OpenUserArnav'},
+    {image: 'assests/icons/spotify.png', title: 'Spotify', href: 'https://open.spotify.com/user/stzdaym79wk5kd2upuacqcy5p?si=890bab7ef5164369'},
+    {image: 'assests/icons/youtube.png', title: 'Youtube', href: 'https://www.youtube.com/channel/UCN_JtwOt0V7gwYH3Ilxrxrg'},
+    {image: 'assests/icons/chess.png', title: 'Chess.com', href: 'https://www.chess.com/member/legendarnav'},
+
+]
+
+links.forEach(link => {
+    linksPanel.innerHTML += `
+        <a class="link" href= ${link.href} target=”_blank”>
+            <img src=${link.image} alt="icon">
+            
+            <h1 class="title">${link.title}</h1>
+        </a>
+        `
+});
